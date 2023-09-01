749f25f9102a53b465fe647b1a56e7a69ddf7d46
jethro34 <77173690+jethro34@users.noreply.github.com>
2023-08-31 20:59:04 -0400

    Add files via upload

--- 104_pointer_basics.c
+++ 104_pointer_basics.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-void main()
+int main()
 {
-  printf("Hello C World!");
+    int age = 42;
+    int* pAge = NULL;
+    
+    pAge = &age;
+
+    printf("variable age's address is: %p\n", &age);
+    printf("variable age's value is: %i\n", age);
+
+    printf("pointer pAge's address is: %p\n", &pAge);
+    printf("pointer pAge's value is: %p\n", pAge);
+    printf("pointer pAge's pointed-to value is: %i\n", *pAge);
+
+    return 0;
 }
